f41dee23d6d042426222f1c404fdf2cf720f70e8
rohan <rohan70@gmail.com>
2022-09-17 10:53:12 +0530

    day 6 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/pages/home_page.dart';
 import 'package:flutter_application_2/pages/login_page.dart';
+import 'package:flutter_application_2/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 //cmd + click to see anyones documentation
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
         fontFamily: GoogleFonts.lato().fontFamily,
       ),
+      debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
@@ -38,8 +40,8 @@ class MyApp extends StatelessWidget {
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/Login": (context) => LoginPage()
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,5 @@
+class MyRoutes {
+  // jiska object sirf ek baar banta hain har baar initialize nhi hoti
+  static String loginRoute = "/login";
+  static String homeRoute = "/home";
+}
