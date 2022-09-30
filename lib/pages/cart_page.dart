2a175f5181d4a75b2d11652558c57eb9d1f74ae9
rohan <rohan70@gmail.com>
2022-09-30 15:08:42 +0530

    day21 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -10,7 +10,61 @@ class CartPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
+      ),
+
+      body: Column(
+        children: [
+          _CardList().p32().expand(),
+          Divider(),
+          _CardTotal(),
+        ], //divider ek niche line dikhata hain
       ), //creates white blank page
+    );
+  }
+}
+
+//creating widget for CardTotal
+class _CardTotal extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          "\$9999".text.xl5.color(context.theme.accentColor).make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all(context.theme.buttonColor)),
+            child: "Buy".text.white.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class _CardList extends StatefulWidget {
+  @override
+  State<_CardList> createState() => _CardListState();
+}
+
+class _CardListState extends State<_CardList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done), //done icon right box hota hain
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: "Item 1".text.make(),
+      ),
     );
   }
 }
